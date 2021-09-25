fa404c7d15939569b5cbbe569ba865219eba1135
Jhonny Ninabanda <jeninabandap@uce.edu.ec>
2021-09-24 20:12:15 -0500

    Segundo Reporte

--- src/main/java2/examen/finall/pro/Main.java
+++ src/main/java2/examen/finall/pro/Main.java
@@ -2,6 +2,7 @@ package examen.finall.pro;
 
 import java.util.Scanner;
 
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -9,7 +10,16 @@ public class Main {
 		Scanner Lector = new Scanner(System.in);
 		
 		int opcion =0 ;
+		String marca = "";
+		String modelo = "";
+		int anioFabricacion = 0;
+		String placa ="";
+		double kilometraje = 0;
 		
+		
+		int numVehiculo = 10;
+		
+		Auto Vehiculo[] = new Auto[numVehiculo];
 		
 		System.out.println("Bienvendos a AMBACAR");
 		
@@ -24,6 +34,89 @@ public class Main {
 		
 		
 		if(opcion == 1) {
+			
+			String opc ="";
+		do {
+			
+			
+			
+			System.out.println("a. Auto Nacional");
+			System.out.println("b. Auto Importado");
+			
+			if (opc.equals("a")) {
+				
+				int i = 0; ; 
+				i++;
+				Auto auto = new Auto(marca, modelo, anioFabricacion, placa, kilometraje);
+				Vehiculo[i] = auto;
+				
+				System.out.println("Ingrese la marca del auto: ");
+				marca = Lector.nextLine();
+				
+				System.out.println("Ingrese la modelo del auto: ");
+				modelo = Lector.nextLine();
+				
+				System.out.println("Ingrese la año fabricacion  del auto: ");
+				anioFabricacion = Lector.nextInt();
+				
+				System.out.println("Ingrese la placa del auto: ");
+				placa = Lector.nextLine();
+				
+				System.out.println("Ingrese la kilometraje del auto: ");
+				kilometraje = Lector.nextDouble();
+				
+				System.out.println("Ingrese codigo nacional:  ");
+				String codigo = Lector.nextLine();
+				
+				auto.setMarca(marca);
+				auto.setModelo(modelo);
+				auto.setAnioFabricacion(anioFabricacion);
+				auto.setPlaca(placa);
+				auto.setKilometraje(kilometraje);
+				
+				
+			}else if (opc.equals("b")) {
+				
+				int i = 0; ; 
+				i++;
+				Auto auto = new Auto(marca, modelo, anioFabricacion, placa, kilometraje);
+				Vehiculo[i] = auto;
+				
+				System.out.println("Ingrese la marca del auto: ");
+				marca = Lector.nextLine();
+				
+				System.out.println("Ingrese la modelo del auto: ");
+				modelo = Lector.nextLine();
+				
+				System.out.println("Ingrese la año fabricacion  del auto: ");
+				anioFabricacion = Lector.nextInt();
+				
+				System.out.println("Ingrese la placa del auto: ");
+				placa = Lector.nextLine();
+				
+				System.out.println("Ingrese la kilometraje del auto: ");
+				kilometraje = Lector.nextDouble();
+				
+				System.out.println("Ingrese codigo nacional:  ");
+				String codigo = Lector.nextLine();
+				
+				auto.setMarca(marca);
+				auto.setModelo(modelo);
+				auto.setAnioFabricacion(anioFabricacion);
+				auto.setPlaca(placa);
+				auto.setKilometraje(kilometraje);
+				
+				
+				
+			}
+			
+			
+		}while(opc.equals("c"));
+			
+			
+			
+
+			
 			
 		}else if(opcion == 2) {
 			
